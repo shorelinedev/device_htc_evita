0f636883a34bdf3505e957d57aaf902bbd1665d9
Matt Mower <mowerm@gmail.com>
2014-05-07 12:35:33 -0500

    evita: remove remount.qcom fstab from makefile
    * Fixes building of eng recoveries
    
    Change-Id: I389e6c0ee05a28a4f3f8fee83ec4e938f9bea7c2
    
    Conflicts:
    	rootdir/Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,17 +17,10 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= remount.qcom
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/remount.qcom
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE    := twrp.fstab
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES    := etc/twrp.fstab
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
