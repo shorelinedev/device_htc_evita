b1957ab7571ceab465cc8e8ac9584d09936211b7
Ethan Chen <intervigil@gmail.com>
2014-07-07 12:00:07 -0700

    evita: Move custom recovery font to device tree
    Change-Id: I315e03a707efcf436823042f75ae8ecd65d81ce9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,7 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_VOLD_MAX_PARTITIONS := 36
 
 # Recovery
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 TARGET_RECOVERY_FSTAB := device/htc/evita/rootdir/etc/fstab.qcom
 
 # TWRP
